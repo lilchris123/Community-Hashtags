08bc6c28c949d757fddce248529abd1af89ea602
Chris Mayol <drlilchris@gmail.com>
2020-06-12 19:29:29 -0400

    fix missing .com in production api_hostname

--- client/src/shared/constants/api.js
+++ client/src/shared/constants/api.js
@@ -1,5 +1,5 @@
 const isProduction= process.env.NODE_ENV ==='production';
-const API_HOSTNAME= isProduction ? 'communityhashtags.herokuapp' :'localhost';
+const API_HOSTNAME= isProduction ? 'communityhashtags.herokuapp.com' :'localhost';
 const PORT= 8080;
 export const API_URL= isProduction ? `https://${API_HOSTNAME}/api`: `http://${API_HOSTNAME}:${PORT}/api` ;
 
