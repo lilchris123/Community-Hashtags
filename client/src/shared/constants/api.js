b7c026cab8ce13e5eec4c37eb7567d1e9b61b322
Chris Mayol <drlilchris@gmail.com>
2022-08-14 14:56:18 -0400

    prod changes

--- client/src/shared/constants/api.js
+++ client/src/shared/constants/api.js
@@ -1,12 +1,15 @@
-const {REACT_APP_API_PORT} = process.env;
-// const isProduction= NODE_ENV ==='production';
+const { REACT_APP_API_HOSTNAME, REACT_APP_API_PORT, NODE_ENV } = process.env;
+const isProduction = NODE_ENV === "production";
 // const REACT_APP_API_HOSTNAME= isProduction ? 'communityhashtags.herokuapp.com' :'localhost';
-// export const API_URL= isProduction ? `https://${API_HOSTNAME}/api`: `http://${API_HOSTNAME}:${REACT_APP_API_PORT}/api` ;
-export const API_URL= `http://localhost:${REACT_APP_API_PORT}/api`;
+console.log(`${REACT_APP_API_HOSTNAME } ${ REACT_APP_API_PORT}`);
+export const API_URL = isProduction
+  ? `http://${REACT_APP_API_HOSTNAME}:${REACT_APP_API_PORT}/api`
+  : `http://localhost:${REACT_APP_API_PORT}/api`;
 
-export const addTokenConfig= ()=>{
-   return {
-   headers: {
-        Authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}`
-    }}
-}
+export const addTokenConfig = () => {
+  return {
+    headers: {
+      Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+    },
+  };
+};
